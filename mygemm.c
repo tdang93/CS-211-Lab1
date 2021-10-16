0fedf7c8943407cc56834a6d5c5ecf3292c79716
tdang93 <aznnoodle101@hotmail.com>
2021-10-15 23:48:34 -0700

    part 2 optimal

--- mygemm.c
+++ mygemm.c
@@ -263,5 +263,33 @@ void bkji(const double *A, const double *B, double *C, const int n, const int b)
 //Cache Reuse part 4
 void optimal(const double* A, const double* B, double *C, const int n, const int b)
 {
-    
+  int i, j, k = 0;
+  int i1, j1, k1 = 0;
+  for (i = 0; i < n; i+=b)
+    for (j = 0; j < n; j+=b)
+      for (k = 0; k < n; k+=b)
+      /* B x B mini matrix multiplications */
+        for(i1 = 0; i1 < i+b; i1 += 2)
+          for(j1 = 0; j1 < j+b; j1 += 2)  {
+            register int t = i1*n+j1; register int tt = t+n; 
+            register double c00 = C[t]; register double c01 = C[t+1];  register double c10 = C[tt]; register double c11 = C[tt+1];
+
+            for(k1 = 0; k1 < k+b; k1 += 2) {
+                /* 2 by 2 mini matrix multiplication using registers*/
+
+                register int ta = i1*n+k1; register int tta = ta+n; register int tb = k1*n+j1; register int ttb = tb+n;
+                register double a00 = A[ta]; register double a01 = A[ta+1]; register double a10 = A[tta]; register double a11 = A[tta+1];
+                register double b00 = B[tb]; register double b01 = B[tb+1]; register double b10 = B[ttb]; register double b11 = B[ttb+1];
+
+                c00 += a00*b00 + a01*b10;
+                c01 += a00*b01 + a01*b11;
+                c10 += a10*b00 + a11*b10;
+                c11 += a10*b01 + a11*b11;
+             }
+
+             C[t] = c00;
+             C[t+1] = c01;
+             C[tt] = c10;
+             C[tt+1] = c11;
+        }
 }
